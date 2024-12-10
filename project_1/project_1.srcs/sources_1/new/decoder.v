ec7395c08cf218586a873e038e613dd0338ea2e4
maru <1843167357@qq.com>
2024-12-10 10:29:14 +0800

    12.10

---
+++ project_1/project_1.srcs/sources_1/new/decoder.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/12/04 17:05:35
+// Design Name: 
+// Module Name: test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test(
+    input a,
+    input b
+    );
+endmodule
